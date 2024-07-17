196328bc43d84d407fd53aec8251bea364c5811f
Rafael Deroncio <rafael.deroncio@hotmail.com>
2024-07-17 20:07:09 -0300

    add GlobalHandlerExceptionMiddleware

---
+++ src/MovieMania.API/Middlewares/GlobalHandlerExceptionMiddleware.cs
@@ -0,0 +1,25 @@
+﻿namespace MovieMania.API.Middlewares;
+
+public class GlobalHandlerExceptionMiddleware
+{
+    private readonly ILogger<GlobalHandlerExceptionMiddleware> _logger;
+    private readonly RequestDelegate _next;
+
+    public GlobalHandlerExceptionMiddleware(RequestDelegate next, ILogger<GlobalHandlerExceptionMiddleware> logger)
+    {
+        _logger = logger;
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception)
+        {
+
+        }
+    }
+}
