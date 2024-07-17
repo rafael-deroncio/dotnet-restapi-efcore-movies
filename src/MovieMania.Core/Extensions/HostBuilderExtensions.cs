dbed6be01252b0daa0f239d3cb90fab9cfb997af
Rafael Deroncio <rafael.deroncio@hotmail.com>
2024-07-17 20:06:30 -0300

    add HostBuilderExtensions and libs

---
+++ src/MovieMania.Core/Extensions/HostBuilderExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Serilog;
+using Serilog.Core;
+
+namespace MovieMania.Core.Extensions;
+
+public static class HostBuilderExtensions
+{
+    public static IHostBuilder ConfigureSerilog(this IHostBuilder hostBuilder)
+    {
+        hostBuilder.UseSerilog((context, config) =>
+            config.MinimumLevel.Information()
+                  .MinimumLevel.ControlledBy(new LoggingLevelSwitch())
+                  .Enrich.WithProperty("Application", context.Configuration["ApplicationName"])
+                  .Enrich.WithProperty("Envioroment", Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT"))
+                  .Enrich.FromLogContext()
+                  .WriteTo.Console()
+                  );
+        return hostBuilder;
+    }
+}
