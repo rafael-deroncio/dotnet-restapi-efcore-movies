6c5174aee4f600f4c6a58901bb2bc2593966db98
Rafael Deroncio <rafael.deroncio@hotmail.com>
2024-07-18 20:51:46 -0300

    add KeywordModel

---
+++ src/MovieMania.Core/Models/KeywordModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using MovieMania.Core.Configurations.DTOs;
+
+namespace MovieMania.Core.Models;
+
+public record KeywordModel : ContextBaseDTO
+{
+    [Key]
+    [Column("keyword_id")]
+    public int KeywordId { get; set; }
+
+    [Required]
+    [StringLength(100)]
+    [Column("keyword_name")]
+    public string Name { get; set; }
+
+    public ICollection<MovieKeywordModel> MovieKeywords { get; set; }
+}
---
+++ src/MovieMania.Core/Models/MovieKeywordModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using MovieMania.Core.Configurations.DTOs;
+
+namespace MovieMania.Core.Models;
+
+public record MovieKeywordModel : ContextBaseDTO
+{
+    [ForeignKey("Movie")]
+    [Column("movie_id")]
+    public int MovieId { get; set; }
+
+    [ForeignKey("Keyword")]
+    [Column("keyword_id")]
+    public int KeywordId { get; set; }
+
+    public MovieModel Movie { get; set; }
+    public KeywordModel Keyword { get; set; }
+}
