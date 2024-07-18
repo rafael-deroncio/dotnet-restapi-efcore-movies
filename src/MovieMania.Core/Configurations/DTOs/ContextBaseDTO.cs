8fe1d3f2970f92e41555d92f21481a2c916aa622
Rafael Deroncio <rafael.deroncio@hotmail.com>
2024-07-18 20:49:12 -0300

    add ContextBaseDTO

--- src/MovieMania.Core/Configurations/DTOs/ContextBaseDTO.cs
+++ src/MovieMania.Core/Configurations/DTOs/ContextBaseDTO.cs
@@ -1,7 +1,11 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace MovieMania.Core.Configurations.DTOs;
 
 public record ContextBaseDTO
 {
+    [Column("created_at")]
     public DateTime CreatedAt { get; set; } = DateTime.Now;
+    [Column("updated_at")]
     public DateTime UpdatedAt { get; set; } = DateTime.Now;
 }
