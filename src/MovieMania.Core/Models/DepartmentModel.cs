2a06a06fdf923bae478dec0f3430905ddbebe62b
Rafael Deroncio <rafael.deroncio@hotmail.com>
2024-07-18 20:50:25 -0300

    add DepartmentModel

---
+++ src/MovieMania.Core/Models/DepartmentModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using MovieMania.Core.Configurations.DTOs;
+
+namespace MovieMania.Core.Models;
+
+public record DepartmentModel : ContextBaseDTO
+{
+    [Key]
+    [Column("department_id")]
+    public int DepartmentId { get; set; }
+
+    [Required]
+    [StringLength(100)]
+    [Column("department_name")]
+    public string Name { get; set; }
+
+    public ICollection<MovieCrewModel> MovieCrews { get; set; }
+}
+
---
+++ src/MovieMania.Core/Models/MovieCrewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using MovieMania.Core.Configurations.DTOs;
+
+namespace MovieMania.Core.Models;
+
+public record MovieCrewModel : ContextBaseDTO
+{
+    [ForeignKey("Movie")]
+    [Column("movie_id")]
+    public int MovieId { get; set; }
+
+    [ForeignKey("Person")]
+    [Column("person_id")]
+    public int PersonId { get; set; }
+
+    [ForeignKey("Department")]
+    [Column("department_id")]
+    public int DepartmentId { get; set; }
+
+    [StringLength(255)]
+    [Column("job")]
+    public string Job { get; set; }
+
+    public MovieModel Movie { get; set; }
+    public PersonModel Person { get; set; }
+    public DepartmentModel Department { get; set; }
+}
+
