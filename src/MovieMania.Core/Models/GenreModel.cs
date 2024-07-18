a8f6368d07e7eb668a7a41e991755e56df1df71f
Rafael Deroncio <rafael.deroncio@hotmail.com>
2024-07-18 20:51:20 -0300

    add GenreModel

---
+++ src/MovieMania.Core/Models/GenreModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using MovieMania.Core.Configurations.DTOs;
+
+namespace MovieMania.Core.Models;
+
+public record GenreModel : ContextBaseDTO
+{
+    [Key]
+    [Column("genre_id")]
+    public int GenreId { get; set; }
+
+    [Required]
+    [StringLength(100)]
+    [Column("name")]
+    public string Name { get; set; }
+
+    public ICollection<MovieGenreModel> MovieGenres { get; set; }
+}
---
+++ src/MovieMania.Core/Models/MovieGenreModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using MovieMania.Core.Configurations.DTOs;
+
+namespace MovieMania.Core.Models;
+
+public record MovieGenreModel : ContextBaseDTO
+{
+    [ForeignKey("Movie")]
+    [Column("movie_id")]
+    public int MovieId { get; set; }
+
+    [ForeignKey("Genre")]
+    [Column("genre_id")]
+    public int GenreId { get; set; }
+
+    public MovieModel Movie { get; set; }
+    public GenreModel Genre { get; set; }
+}
