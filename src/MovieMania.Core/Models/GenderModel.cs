dadd0bc1d25948240880567ec235f2787b2244b2
Rafael Deroncio <rafael.deroncio@hotmail.com>
2024-07-18 20:50:59 -0300

    add GenderModel

---
+++ src/MovieMania.Core/Models/GenderModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using MovieMania.Core.Configurations.DTOs;
+
+namespace MovieMania.Core.Models;
+
+public record GenderModel : ContextBaseDTO
+{
+    [Key]
+    [Column("gender_id")]
+    public int GenderId { get; set; }
+
+    [Required]
+    [StringLength(50)]
+    [Column("gender")]
+    public string Gender { get; set; }
+
+    public ICollection<MovieCastModel> MovieCasts { get; set; }
+}
---
+++ src/MovieMania.Core/Models/MovieCastModel.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using MovieMania.Core.Configurations.DTOs;
+
+namespace MovieMania.Core.Models;
+
+public record MovieCastModel : ContextBaseDTO
+{
+    [ForeignKey("Movie")]
+    [Column("movie_id")]
+    public int MovieId { get; set; }
+
+    [ForeignKey("Gender")]
+    [Column("gender_id")]
+    public int GenderId { get; set; }
+
+    [ForeignKey("Person")]
+    [Column("person_id")]
+    public int PersonId { get; set; }
+
+    [StringLength(255)]
+    [Column("character_name")]
+    public string CharacterName { get; set; }
+
+    [Column("cast_order")]
+    public int CastOrder { get; set; }
+
+    public MovieModel Movie { get; set; }
+    public GenderModel Gender { get; set; }
+    public PersonModel Person { get; set; }
+}
