b0ef0d7b393a9327ff2801382d9d779a8b77a0c4
Rafael Deroncio <rafael.deroncio@hotmail.com>
2024-07-21 19:28:09 -0300

    Add responsesfor exception in domain

---
+++ src/MovieMania.Domain/Responses/ExceptionResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace MovieMania.Domain.Responses;
+
+public record ExceptionResponse
+{
+    public string Title { get; set; }
+    public string Type { get; set; }
+    public string[] Messages { get; set; }
+}
