358e2c6cab9d37570a7e4dc5b1be64ac9543cd9d
Rafael Deroncio <rafael.deroncio@hotmail.com>
2024-07-21 19:29:00 -0300

    implement GlobalHandlerExceptionMiddleware

--- src/MovieMania.API/Middlewares/GlobalHandlerExceptionMiddleware.cs
+++ src/MovieMania.API/Middlewares/GlobalHandlerExceptionMiddleware.cs
@@ -1,13 +1,17 @@
-﻿namespace MovieMania.API.Middlewares;
+﻿using System.Net;
+using System.Text.Json;
+using MovieMania.Core.Exceptions;
+using MovieMania.Domain.Enums;
+using MovieMania.Domain.Responses;
+
+namespace MovieMania.API.Middlewares;
 
 public class GlobalHandlerExceptionMiddleware
 {
-    private readonly ILogger<GlobalHandlerExceptionMiddleware> _logger;
     private readonly RequestDelegate _next;
 
-    public GlobalHandlerExceptionMiddleware(RequestDelegate next, ILogger<GlobalHandlerExceptionMiddleware> logger)
+    public GlobalHandlerExceptionMiddleware(RequestDelegate next)
     {
-        _logger = logger;
         _next = next;
     }
 
@@ -17,9 +21,35 @@ public class GlobalHandlerExceptionMiddleware
         {
             await _next(context);
         }
+        catch (BaseException ex)
+        {
+            ExceptionResponse response = new()
+            {
+                Title = ex.Title,
+                Type = ex.Type.GetDescription(),
+                Messages = [ex.Message]
+            };
+
+            string json = JsonSerializer.Serialize(response);
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            await context.Response.WriteAsync(json);
+        }
         catch (Exception)
         {
+            ExceptionResponse response = new()
+            {
+                Title = "Internal Error",
+                Type = ResponseType.Fatal.GetDescription(),
+                Messages = ["An error occurred while processing the request."]
+            };
 
+            string json = JsonSerializer.Serialize(response);
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            await context.Response.WriteAsync(json);
         }
     }
 }
