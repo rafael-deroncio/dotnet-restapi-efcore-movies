49d4787918973a8873128ff0688a5e7d1be26a84
Rafael Deroncio <rafael.deroncio@hotmail.com>
2024-07-21 19:28:28 -0300

    implement EnumExtension

---
+++ src/MovieMania.Core/Extensions/EnumExtension.cs
@@ -0,0 +1,13 @@
+﻿namespace MovieMania.Core;
+
+public static class EnumExtension
+{
+    public static string GetDescription(this Enum value)
+    {
+        System.Reflection.FieldInfo fieldInfo = value.GetType().GetField(value.ToString());
+
+        DescriptionAttribute attribute = (DescriptionAttribute)Attribute.GetCustomAttribute(fieldInfo, typeof(DescriptionAttribute));
+
+        return attribute == null ? value.ToString() : attribute.Description;
+    }
+}
