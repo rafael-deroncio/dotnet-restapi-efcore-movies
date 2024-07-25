648611046d39ab07f54f78541cf566507883736f
Rafael Deroncio <rafael.deroncio@hotmail.com>
2024-07-24 23:29:11 -0300

    implement DatabaseMemory

---
+++ src/MovieMania.Core/Repositories/DatabaseMemory.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieMania.Core.Contexts;
+using MovieMania.Core.Contexts.Entities;
+using MovieMania.Core.Repositories.Interfaces;
+
+namespace MovieMania.Core.Repositories;
+
+public class DatabaseMemory : IDatabaseMemory
+{
+    private readonly MovieManiaContext _context;
+    private List<CountryEntity> _countries;
+    private List<DepartmentEntity> _departments;
+    private List<GenderEntity> _genders;
+    private List<GenreEntity> _genres;
+    private List<KeywordEntity> _keywords;
+    private List<LanguageEntity> _languages;
+    private List<LanguageRoleEntity> _languageRoles;
+    private List<ProductionCompanyEntity> _productionCompanies;
+
+    public DatabaseMemory(MovieManiaContext context)
+    {
+        this._context = context;
+        Start().GetAwaiter().GetResult();
+    }
+
+    public IEnumerable<CountryEntity> Countries { get { return this._countries; } }
+    public IEnumerable<DepartmentEntity> Departments { get { return this._departments; } }
+    public IEnumerable<GenderEntity> Genders { get { return this._genders; } }
+    public IEnumerable<GenreEntity> Genres { get { return this._genres; } }
+    public IEnumerable<KeywordEntity> Keywords { get { return this._keywords; } }
+    public IEnumerable<LanguageEntity> Languages { get { return this._languages; } }
+    public IEnumerable<LanguageRoleEntity> LanguageRoles { get { return this._languageRoles; } }
+    public IEnumerable<ProductionCompanyEntity> ProductionCompanies { get { return this._productionCompanies; } }
+
+
+    private async Task Start()
+    {
+        await UpdateCountries();
+        await UpdateDepartments();
+        await UpdateGenders();
+        await UpdateGenres();
+        await UpdateKeywords();
+        await UpdateLanguages();
+        await UpdateLanguageRoles();
+        await UpdateProductionCompanies();
+    }
+
+    public async Task UpdateCountries()
+    {
+        _countries = await _context.Countries.ToListAsync();
+    }
+
+    public async Task UpdateDepartments()
+    {
+        _departments = await _context.Departments.ToListAsync();
+    }
+
+    public async Task UpdateGenders()
+    {
+        _genders = await _context.Genders.ToListAsync();
+    }
+
+    public async Task UpdateGenres()
+    {
+        _genres = await _context.Genres.ToListAsync();
+    }
+
+    public async Task UpdateKeywords()
+    {
+        _keywords = await _context.Keywords.ToListAsync();
+    }
+
+    public async Task UpdateLanguages()
+    {
+        _languages = await _context.Languages.ToListAsync();
+    }
+
+    public async Task UpdateLanguageRoles()
+    {
+        _languageRoles = await _context.LanguageRoles.ToListAsync();
+    }
+
+    public async Task UpdateProductionCompanies()
+    {
+        _productionCompanies = await _context.ProductionCompanies.ToListAsync();
+    }
+}
---
+++ src/MovieMania.Core/Repositories/Interfaces/IDatabaseMemory.cs
@@ -0,0 +1,106 @@
+﻿using MovieMania.Core.Contexts.Entities;
+
+namespace MovieMania.Core.Repositories.Interfaces;
+
+public interface IDatabaseMemory
+{
+    /// <summary>
+    /// Interface for managing in-memory database operations.
+    /// </summary>
+    public interface IDatabaseMemory
+    {
+        /// <summary>
+        /// Gets the list of countries.
+        /// </summary>
+        IEnumerable<CountryEntity> Countries { get; }
+
+        /// <summary>
+        /// Gets the list of departments.
+        /// </summary>
+        IEnumerable<DepartmentEntity> Departments { get; }
+
+        /// <summary>
+        /// Gets the list of genders.
+        /// </summary>
+        IEnumerable<GenderEntity> Genders { get; }
+
+        /// <summary>
+        /// Gets the list of genres.
+        /// </summary>
+        IEnumerable<GenreEntity> Genres { get; }
+
+        /// <summary>
+        /// Gets the list of keywords.
+        /// </summary>
+        IEnumerable<KeywordEntity> Keywords { get; }
+
+        /// <summary>
+        /// Gets the list of languages.
+        /// </summary>
+        IEnumerable<LanguageEntity> Languages { get; }
+
+        /// <summary>
+        /// Gets the list of language roles.
+        /// </summary>
+        IEnumerable<LanguageRoleEntity> LanguageRoles { get; }
+
+        /// <summary>
+        /// Gets the list of production companies.
+        /// </summary>
+        IEnumerable<ProductionCompanyEntity> ProductionCompanies { get; }
+
+        /// <summary>
+        /// Starts the process of updating the in-memory database.
+        /// </summary>
+        /// <returns>A task that represents the asynchronous operation.</returns>
+        Task Start();
+
+        /// <summary>
+        /// Updates the list of countries.
+        /// </summary>
+        /// <returns>A task that represents the asynchronous operation.</returns>
+        Task UpdateCountries();
+
+        /// <summary>
+        /// Updates the list of departments.
+        /// </summary>
+        /// <returns>A task that represents the asynchronous operation.</returns>
+        Task UpdateDepartments();
+
+        /// <summary>
+        /// Updates the list of genders.
+        /// </summary>
+        /// <returns>A task that represents the asynchronous operation.</returns>
+        Task UpdateGenders();
+
+        /// <summary>
+        /// Updates the list of genres.
+        /// </summary>
+        /// <returns>A task that represents the asynchronous operation.</returns>
+        Task UpdateGenres();
+
+        /// <summary>
+        /// Updates the list of keywords.
+        /// </summary>
+        /// <returns>A task that represents the asynchronous operation.</returns>
+        Task UpdateKeywords();
+
+        /// <summary>
+        /// Updates the list of languages.
+        /// </summary>
+        /// <returns>A task that represents the asynchronous operation.</returns>
+        Task UpdateLanguages();
+
+        /// <summary>
+        /// Updates the list of language roles.
+        /// </summary>
+        /// <returns>A task that represents the asynchronous operation.</returns>
+        Task UpdateLanguageRoles();
+
+        /// <summary>
+        /// Updates the list of production companies.
+        /// </summary>
+        /// <returns>A task that represents the asynchronous operation.</returns>
+        Task UpdateProductionCompanies();
+    }
+}
