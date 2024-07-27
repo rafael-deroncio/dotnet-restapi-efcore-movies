7677e579298e6881f83a75f6088837f721473723
Rafael Deroncio <rafael.deroncio@hotmail.com>
2024-07-26 23:40:47 -0300

    add CountryRequest

---
+++ src/MovieMania.Domain/Requests/CountryRequest.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace MovieMania.Domain.Requests;
+
+public record CountryRequest
+{
+    [Required]
+    [StringLength(10)]
+    [JsonPropertyName("iso_code")]
+    public string IsoCode { get; set; }
+
+    [Required]
+    [StringLength(100)]
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+}
