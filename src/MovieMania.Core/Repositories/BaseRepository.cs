236d2a2c74f444217a1312e02720bdf67f689e18
Rafael Deroncio <rafael.deroncio@hotmail.com>
2024-07-26 23:42:57 -0300

    update IDatabaseMemory interface and refactor BaseRepository

--- src/MovieMania.Core/Repositories/BaseRepository.cs
+++ src/MovieMania.Core/Repositories/BaseRepository.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Metadata;
 using MovieMania.Core.Configurations.DTOs;
 using MovieMania.Core.Extensions;
 using MovieMania.Core.Repositories.Interfaces;
@@ -12,10 +13,16 @@ public class BaseRepository<TEntity>(DbContext context) : IBaseRepository<TEntit
     private readonly DbSet<TEntity> _entity = context.GetEntity<TEntity>();
 
     public async Task<TEntity> Get(TEntity entity)
-        => await _entity.FindAsync(entity);
+    {
+        IProperty property = _context.Model.FindEntityType(typeof(TEntity)).FindPrimaryKey().Properties.FirstOrDefault()
+            ?? throw new InvalidOperationException("No primary key defined for entity.");
+
+        return await _entity.FindAsync(property.PropertyInfo.GetValue(entity));
+    }
 
     public async Task<TEntity> Create(TEntity entity)
     {
+        entity.UpdatedAt = entity.CreatedAt = DateTime.UtcNow;
         EntityEntry<TEntity> result = await _entity.AddAsync(entity);
         await _context.SaveChangesAsync();
         return result.Entity;
@@ -23,6 +30,7 @@ public class BaseRepository<TEntity>(DbContext context) : IBaseRepository<TEntit
 
     public async Task<TEntity> Update(TEntity entity)
     {
+        entity.UpdatedAt = DateTime.UtcNow;
         EntityEntry<TEntity> result = _entity.Update(entity);
         await _context.SaveChangesAsync();
         return result.Entity;
