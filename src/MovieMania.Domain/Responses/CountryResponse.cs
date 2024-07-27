b9665039ea2da4c4aa9fce8f5452b7c31fffae44
Rafael Deroncio <rafael.deroncio@hotmail.com>
2024-07-26 23:41:02 -0300

    add CountryResponse

---
+++ src/MovieMania.Domain/Responses/CountryResponse.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json.Serialization;
+
+namespace MovieMania.Domain.Responses;
+
+public record CountryResponse
+{
+    public int Id { get; set; }
+
+    [JsonPropertyName("iso_code")]
+    public string IsoCode { get; set; }
+
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+
+    [JsonPropertyName("created_at")]
+    public DateTime CreatedAt { get; set; }
+}
