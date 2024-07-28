3dc108770eeb8cb49f7a25017f419fff80d48cf8
Rafael Deroncio <rafael.deroncio@hotmail.com>
2024-07-28 13:51:04 -0300

    Add validation in request for pagination

--- src/MovieMania.Domain/Requests/PaginationRequest.cs
+++ src/MovieMania.Domain/Requests/PaginationRequest.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace MovieMania.Domain.Requests;
 
@@ -7,17 +8,30 @@ namespace MovieMania.Domain.Requests;
 /// </summary>
 public record PaginationRequest
 {
+    private int _page;
+    private int _size;
+
     /// <summary>
     /// Gets or sets the page number for the paginated request.
     /// This property maps to the JSON property 'page'.
     /// </summary>
     [JsonPropertyName("page")]
-    public int Page { get; set; }
+    public int Page
+    {
+        get => _page;
+        set => _page = value <= 0 ? 1 : value;
+    }
+
 
     /// <summary>
     /// Gets or sets the size of the page (number of items per page) for the paginated request.
     /// This property maps to the JSON property 'size'.
     /// </summary>
     [JsonPropertyName("size")]
-    public int Size { get; set; }
+    public int Size
+    {
+        get => _size;
+        set => _size =  value <= 0 ? 1 :
+                        value > 10 ? 10 : value;
+    }
 }
