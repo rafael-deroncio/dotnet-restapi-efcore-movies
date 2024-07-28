3aa76190596041b8629b09a7a630be8efa411eba
Rafael Deroncio <rafael.deroncio@hotmail.com>
2024-07-28 13:32:31 -0300

    implement EntityBadRequestException

---
+++ src/MovieMania.Core/Exceptions/EntityBadRequestException.cs
@@ -0,0 +1,14 @@
+﻿using System.Net;
+using MovieMania.Domain.Enums;
+
+namespace MovieMania.Core.Exceptions;
+
+public class EntityBadRequestException : BaseException
+{
+    public EntityBadRequestException(string title, string message) : base(title, message, HttpStatusCode.NotFound)
+    {
+        Title = title;
+        Code = HttpStatusCode.BadRequest;
+        Type =  ResponseType.Error;
+    }
+}
