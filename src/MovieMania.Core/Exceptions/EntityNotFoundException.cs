0e8642c02e25d74f533a869957eac8db1e5b3ee3
Rafael Deroncio <rafael.deroncio@hotmail.com>
2024-07-28 13:32:52 -0300

    implement EntityNotFoundException

---
+++ src/MovieMania.Core/Exceptions/EntityNotFoundException.cs
@@ -0,0 +1,14 @@
+﻿using System.Net;
+using MovieMania.Domain.Enums;
+
+namespace MovieMania.Core.Exceptions;
+
+public class EntityNotFoundException : BaseException
+{
+    public EntityNotFoundException(string title, string message) : base(title, message, HttpStatusCode.NotFound)
+    {
+        Title = title;
+        Code = HttpStatusCode.NotFound;
+        Type =  ResponseType.Error;
+    }
+}
