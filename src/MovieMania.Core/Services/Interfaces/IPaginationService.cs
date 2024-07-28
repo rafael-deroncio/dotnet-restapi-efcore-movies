0bc3095d6bb83880151bcecb26a4d5f8634ebc73
Rafael Deroncio <rafael.deroncio@hotmail.com>
2024-07-28 13:42:38 -0300

    add doc IPaginationService

--- src/MovieMania.Core/Services/Interfaces/IPaginationService.cs
+++ src/MovieMania.Core/Services/Interfaces/IPaginationService.cs
@@ -3,7 +3,16 @@ using MovieMania.Domain.Responses;
 
 namespace MovieMania.Core.Services.Interfaces;
 
+/// <summary>
+/// Interface for pagination services.
+/// </summary>
 public interface IPaginationService
 {
+    /// <summary>
+    /// Retrieves a paginated response based on the specified pagination request.
+    /// </summary>
+    /// <typeparam name="T">The type of the items in the pagination request and response.</typeparam>
+    /// <param name="request">The pagination request containing the paging parameters and filtering criteria.</param>
+    /// <returns>A task representing the asynchronous operation. The task result contains the paginated response.</returns>
     Task<PaginationResponse<T>> GetPagination<T>(PaginationRequest<T> request);
 }
