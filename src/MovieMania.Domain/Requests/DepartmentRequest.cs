d2fe2c3eae8a7d2c65a9f1ddc7276c34254c29b7
Rafael Deroncio <rafael.deroncio@hotmail.com>
2024-07-29 19:01:28 -0300

    add Department request and response

---
+++ src/MovieMania.Domain/Requests/DepartmentRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace MovieMania.Domain.Requests;
+
+public record DepartmentRequest
+{
+    [Required]
+    [StringLength(100)]
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+}
---
+++ src/MovieMania.Domain/Responses/DepartmentResponse.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+
+namespace MovieMania.Domain.Responses;
+
+public record DepartmentResponse
+{
+    public int Id { get; set; }
+
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+
+    [JsonPropertyName("created_at")]
+    public DateTime CreatedAt { get; set; }
+}
