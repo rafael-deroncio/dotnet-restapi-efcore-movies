7b5c2b711aa6ddcb3de0f03833db05b32bd749f6
Rafael Deroncio <rafael.deroncio@hotmail.com>
2024-07-29 20:30:33 -0300

    add gender request response

---
+++ src/MovieMania.Domain/Requests/GenderRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace MovieMania.Domain.Requests;
+
+public record GenderRequest
+{
+    [Required]
+    [StringLength(100)]
+    [JsonPropertyName("gender")]
+    public string Gender { get; set; }
+}
---
+++ src/MovieMania.Domain/Responses/GenderResponse.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+
+namespace MovieMania.Domain.Responses;
+
+public record GenderResponse
+{
+    public int Id { get; set; }
+
+    [JsonPropertyName("gender")]
+    public string Gender { get; set; }
+
+    [JsonPropertyName("created_at")]
+    public DateTime CreatedAt { get; set; }
+}
