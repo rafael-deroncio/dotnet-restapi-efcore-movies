e760206f5bc281102b7e7553ddb11d5c0e4caa9d
Rafael Deroncio <rafael.deroncio@hotmail.com>
2024-07-29 19:28:49 -0300

    implement uses of services in DepartamentController

--- src/MovieMania.API/Controllers/DepartamentController.cs
+++ src/MovieMania.API/Controllers/DepartamentController.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using MovieMania.Core.Requests;
+using MovieMania.Core.Services.Interfaces;
+using MovieMania.Domain.Requests;
+using MovieMania.Domain.Responses;
 
 namespace DepartamentMania.API.Controllers;
 
@@ -7,30 +11,46 @@ namespace DepartamentMania.API.Controllers;
 [ApiVersion("1.0")]
 [ApiController]
 [Authorize]
-public class DepartamentController : Controller
+public class DepartamentController(IDepartmentService service) : Controller
 {
+    private readonly IDepartmentService _service = service;
+
     [HttpGet("paged")]
+    [ProducesResponseType(typeof(PaginationResponse<DepartmentResponse>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ExceptionResponse), StatusCodes.Status422UnprocessableEntity)]
     [AllowAnonymous]
-    public async Task<IActionResult> GetDepartaments([FromQuery] int page, [FromQuery] int size)
-        => Ok(await Task.FromResult(new { page, size }));
+    public async Task<IActionResult> GetDepartments([FromQuery] PaginationRequest request)
+        => Ok(await _service.GetPagedDepartments(request));
 
     [HttpGet("{id:int}")]
+    [ProducesResponseType(typeof(DepartmentResponse), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ExceptionResponse), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(ExceptionResponse), StatusCodes.Status422UnprocessableEntity)]
     [AllowAnonymous]
-    public async Task<IActionResult> GetDepartament([FromRoute] int id)
-        => Ok(await Task.FromResult(new { id }));
+    public async Task<IActionResult> GetDepartment([FromRoute] int id)
+        => Ok(await _service.GetDepartmentById(id));
 
-    [HttpPost()]
+    [HttpPost]
+    [ProducesResponseType(typeof(DepartmentResponse), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ExceptionResponse), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(ExceptionResponse), StatusCodes.Status422UnprocessableEntity)]
     [AllowAnonymous]
-    public async Task<IActionResult> PostDepartament([FromBody] object request)
-        => Ok(await Task.FromResult(request));
+    public async Task<IActionResult> PostDepartment([FromBody] DepartmentRequest request)
+        => Ok(await _service.CreateDepartment(request));
 
     [HttpPut("{id:int}")]
+    [ProducesResponseType(typeof(DepartmentResponse), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ExceptionResponse), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(ExceptionResponse), StatusCodes.Status422UnprocessableEntity)]
     [AllowAnonymous]
-    public async Task<IActionResult> PutDepartament([FromRoute] int id, [FromBody] object request)
-        => Ok(await Task.FromResult(new { id, request }));
+    public async Task<IActionResult> PutDepartment([FromRoute] int id, [FromBody] DepartmentRequest request)
+        => Ok(await _service.UpdateDepartment(id, request));
 
     [HttpDelete("{id:int}")]
+    [ProducesResponseType(typeof(bool), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ExceptionResponse), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(ExceptionResponse), StatusCodes.Status422UnprocessableEntity)]
     [AllowAnonymous]
-    public async Task<IActionResult> DeleteDepartament([FromRoute] int id)
-        => Ok(await Task.FromResult(new { id }));
+    public async Task<IActionResult> DeleteDepartment([FromRoute] int id)
+        => Ok(await _service.DeleteDepartment(id));
 }
