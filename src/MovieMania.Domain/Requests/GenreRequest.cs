4c8f533e715a2e58dbefe1af964a1bdc7c61eabc
Rafael Deroncio <rafael.deroncio@hotmail.com>
2024-07-29 19:59:10 -0300

    add genre request and response

---
+++ src/MovieMania.Domain/Requests/GenreRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace MovieMania.Domain.Requests;
+
+public record GenreRequest
+{
+    [Required]
+    [StringLength(100)]
+    [JsonPropertyName("genre")]
+    public string Genre { get; set; }
+}
