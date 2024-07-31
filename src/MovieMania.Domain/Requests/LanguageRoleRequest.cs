8524153f36306f1fa020fc253c0412e233d8b191
Rafael Deroncio <rafael.deroncio@hotmail.com>
2024-07-30 23:03:26 -0300

    add LanguageRole request and response

---
+++ src/MovieMania.Domain/Requests/LanguageRoleRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace MovieMania.Domain.Requests;
+
+public record LanguageRoleRequest
+{
+    [Required]
+    [StringLength(100)]
+    [JsonPropertyName("role")]
+    public string Role { get; set; }
+}
---
+++ src/MovieMania.Domain/Responses/LanguageRoleResponse.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+
+namespace MovieMania.Domain.Responses;
+
+public record LanguageRoleResponse
+{
+    public int Id { get; set; }
+
+    [JsonPropertyName("role")]
+    public string Role { get; set; }
+
+    [JsonPropertyName("created_at")]
+    public DateTime CreatedAt { get; set; }
+}
