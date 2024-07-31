5884dbee32cafe00ae971345c8b44d61b93dece3
Rafael Deroncio <rafael.deroncio@hotmail.com>
2024-07-30 21:31:34 -0300

    implement KeywordController

--- src/MovieMania.API/Controllers/KeywordController.cs
+++ src/MovieMania.API/Controllers/KeywordController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using MovieMania.Core.Services.Interfaces;
+using MovieMania.Domain.Requests;
+using MovieMania.Domain.Responses;
 
 namespace KeywordMania.API.Controllers;
 
@@ -7,30 +10,47 @@ namespace KeywordMania.API.Controllers;
 [ApiVersion("1.0")]
 [ApiController]
 [Authorize]
-public class KeywordController : Controller
+public class KeywordController(IKeywordService service) : Controller
 {
+    private readonly IKeywordService _service = service;
+
     [HttpGet("paged")]
+    [ProducesResponseType(typeof(PaginationResponse<KeywordResponse>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ExceptionResponse), StatusCodes.Status422UnprocessableEntity)]
     [AllowAnonymous]
-    public async Task<IActionResult> GetKeywords([FromQuery] int page, [FromQuery] int size)
-        => Ok(await Task.FromResult(new { page, size }));
+    public async Task<IActionResult> GetKeywords([FromQuery] PaginationRequest request)
+        => Ok(await _service.GetPagedKeywords(request));
 
     [HttpGet("{id:int}")]
+    [ProducesResponseType(typeof(KeywordResponse), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ExceptionResponse), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(ExceptionResponse), StatusCodes.Status422UnprocessableEntity)]
     [AllowAnonymous]
     public async Task<IActionResult> GetKeyword([FromRoute] int id)
-        => Ok(await Task.FromResult(new { id }));
+        => Ok(await _service.GetKeywordById(id));
 
-    [HttpPost()]
+    [HttpPost]
+    [ProducesResponseType(typeof(KeywordResponse), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ExceptionResponse), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(ExceptionResponse), StatusCodes.Status422UnprocessableEntity)]
     [AllowAnonymous]
-    public async Task<IActionResult> PostKeyword([FromBody] object request)
-        => Ok(await Task.FromResult(request));
+    public async Task<IActionResult> PostKeyword([FromBody] KeywordRequest request)
+        => Ok(await _service.CreateKeyword(request));
 
     [HttpPut("{id:int}")]
+    [ProducesResponseType(typeof(KeywordResponse), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ExceptionResponse), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(ExceptionResponse), StatusCodes.Status422UnprocessableEntity)]
     [AllowAnonymous]
-    public async Task<IActionResult> PutKeyword([FromRoute] int id, [FromBody] object request)
-        => Ok(await Task.FromResult(new { id, request }));
+    public async Task<IActionResult> PutKeyword([FromRoute] int id, [FromBody] KeywordRequest request)
+        => Ok(await _service.UpdateKeyword(id, request));
 
     [HttpDelete("{id:int}")]
+    [ProducesResponseType(typeof(bool), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ExceptionResponse), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(ExceptionResponse), StatusCodes.Status422UnprocessableEntity)]
     [AllowAnonymous]
     public async Task<IActionResult> DeleteKeyword([FromRoute] int id)
-        => Ok(await Task.FromResult(new { id }));
+        => Ok(await _service.DeleteKeyword(id));
 }
+
