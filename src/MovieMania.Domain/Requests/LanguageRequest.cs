35ca60318df62102f9c0a19dfc1af8672f422b12
Rafael Deroncio <rafael.deroncio@hotmail.com>
2024-07-30 22:39:56 -0300

    add language request response

---
+++ src/MovieMania.Domain/Requests/LanguageRequest.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace MovieMania.Domain.Requests;
+
+public record LanguageRequest
+{
+    [Required]
+    [StringLength(5)]
+    [JsonPropertyName("code")]
+    public string Code { get; set; }
+
+    [Required]
+    [StringLength(100)]
+    [JsonPropertyName("language")]
+    public string Language { get; set; }
+}
---
+++ src/MovieMania.Domain/Responses/LanguageResponse.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json.Serialization;
+
+namespace MovieMania.Domain.Responses;
+
+public record LanguageResponse
+{
+    public int Id { get; set; }
+
+    [JsonPropertyName("code")]
+    public string Code { get; set; }
+
+    [JsonPropertyName("language")]
+    public string Language { get; set; }
+
+    [JsonPropertyName("created_at")]
+    public DateTime CreatedAt { get; set; }
+}
