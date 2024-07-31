4e202ca6b014700e3148f6db6f0f8c8d8c6c219c
Rafael Deroncio <rafael.deroncio@hotmail.com>
2024-07-30 21:26:01 -0300

    add keyword request and response

---
+++ src/MovieMania.Domain/Requests/KeywordRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace MovieMania.Domain.Requests;
+
+public record KeywordRequest
+{
+    [Required]
+    [StringLength(100)]
+    [JsonPropertyName("keyword")]
+    public string Keyword { get; set; }
+}
---
+++ src/MovieMania.Domain/Responses/KeywordResponse.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+
+namespace MovieMania.Domain.Responses;
+
+public record KeywordResponse
+{
+    public int Id { get; set; }
+
+    [JsonPropertyName("keyword")]
+    public string Keyword { get; set; }
+
+    [JsonPropertyName("created_at")]
+    public DateTime CreatedAt { get; set; }
+}
