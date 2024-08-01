a3ff0929f4464de7380572082ce01130925e034e
Rafael Deroncio <rafael.deroncio@hotmail.com>
2024-07-31 23:52:50 -0300

    implement ProductionCompany controller

--- src/MovieMania.API/Controllers/ProductionCompanyController.cs
+++ src/MovieMania.API/Controllers/ProductionCompanyController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using MovieMania.Core.Services.Interfaces;
+using MovieMania.Domain.Requests;
+using MovieMania.Domain.Responses;
 
 namespace MovieMania.API.Controllers;
 
@@ -7,30 +10,46 @@ namespace MovieMania.API.Controllers;
 [ApiVersion("1.0")]
 [ApiController]
 [Authorize]
-public class ProductionCompanyController : Controller
+public class ProductionCompanyController(IProductionCompanyService service) : Controller
 {
+    private readonly IProductionCompanyService _service = service;
+
     [HttpGet("paged")]
+    [ProducesResponseType(typeof(PaginationResponse<ProductionCompanyResponse>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ExceptionResponse), StatusCodes.Status422UnprocessableEntity)]
     [AllowAnonymous]
-    public async Task<IActionResult> GetProductionCompanies([FromQuery] int page, [FromQuery] int size)
-        => Ok(await Task.FromResult(new { page, size }));
+    public async Task<IActionResult> GetProductionCompanys([FromQuery] PaginationRequest request)
+        => Ok(await _service.GetPagedProductionCompanys(request));
 
     [HttpGet("{id:int}")]
+    [ProducesResponseType(typeof(ProductionCompanyResponse), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ExceptionResponse), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(ExceptionResponse), StatusCodes.Status422UnprocessableEntity)]
     [AllowAnonymous]
     public async Task<IActionResult> GetProductionCompany([FromRoute] int id)
-        => Ok(await Task.FromResult(new { id }));
+        => Ok(await _service.GetProductionCompanyById(id));
 
-    [HttpPost()]
+    [HttpPost]
+    [ProducesResponseType(typeof(ProductionCompanyResponse), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ExceptionResponse), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(ExceptionResponse), StatusCodes.Status422UnprocessableEntity)]
     [AllowAnonymous]
-    public async Task<IActionResult> PostProductionCompany([FromBody] object request)
-        => Ok(await Task.FromResult(request));
+    public async Task<IActionResult> PostProductionCompany([FromBody] ProductionCompanyRequest request)
+        => Ok(await _service.CreateProductionCompany(request));
 
     [HttpPut("{id:int}")]
+    [ProducesResponseType(typeof(ProductionCompanyResponse), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ExceptionResponse), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(ExceptionResponse), StatusCodes.Status422UnprocessableEntity)]
     [AllowAnonymous]
-    public async Task<IActionResult> PutProductionCompany([FromRoute] int id, [FromBody] object request)
-        => Ok(await Task.FromResult(new { id, request }));
+    public async Task<IActionResult> PutProductionCompany([FromRoute] int id, [FromBody] ProductionCompanyRequest request)
+        => Ok(await _service.UpdateProductionCompany(id, request));
 
     [HttpDelete("{id:int}")]
+    [ProducesResponseType(typeof(bool), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ExceptionResponse), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(ExceptionResponse), StatusCodes.Status422UnprocessableEntity)]
     [AllowAnonymous]
     public async Task<IActionResult> DeleteProductionCompany([FromRoute] int id)
-        => Ok(await Task.FromResult(new { id }));
+        => Ok(await _service.DeleteProductionCompany(id));
 }
