babef5e230a011cad1bfb41ed1ffc5f2f1fff9bb
Rafael Deroncio <rafael.deroncio@hotmail.com>
2024-08-11 13:25:42 -0300

    update MovieFilterRequest

--- src/MovieMania.Domain/Requests/MovieFilterRequest.cs
+++ src/MovieMania.Domain/Requests/MovieFilterRequest.cs
@@ -11,47 +11,47 @@ public class MovieFilterRequest
     /// Gets or sets a value indicating whether to include production countries in the response.
     /// </summary>
     [JsonPropertyName("add_production_contries")]
-    public bool AddProductionCountries { get; set; } = true;
+    public bool AddProductionCountries { get; set; } = false;
 
     /// <summary>
     /// Gets or sets a value indicating whether to include companies in the response.
     /// </summary>
     [JsonPropertyName("add_companies")]
-    public bool AddCompanies { get; set; } = true;
+    public bool AddCompanies { get; set; } = false;
 
     /// <summary>
     /// Gets or sets a value indicating whether to include languages in the response.
     /// </summary>
     [JsonPropertyName("add_languages")]
-    public bool AddLanguages { get; set; } = true;
+    public bool AddLanguages { get; set; } = false;
 
     /// <summary>
     /// Gets or sets a value indicating whether to include genres in the response.
     /// </summary>
     [JsonPropertyName("add_genres")]
-    public bool AddGenres { get; set; } = true;
+    public bool AddGenres { get; set; } = false;
 
     /// <summary>
     /// Gets or sets a value indicating whether to include keywords in the response.
     /// </summary>
     [JsonPropertyName("add_keywords")]
-    public bool AddKeywords { get; set; } = true;
+    public bool AddKeywords { get; set; } = false;
 
     /// <summary>
     /// Gets or sets a value indicating whether to include cast members in the response.
     /// </summary>
     [JsonPropertyName("add_casts")]
-    public bool AddCasts { get; set; } = true;
+    public bool AddCasts { get; set; } = false;
 
     /// <summary>
     /// Gets or sets a value indicating whether to include crew members in the response.
     /// </summary>
     [JsonPropertyName("add_crews")]
-    public bool AddCrews { get; set; } = true;
+    public bool AddCrews { get; set; } = false;
 
     /// <summary>
     /// Gets or sets a value indicating whether to include images in the response.
     /// </summary>
     [JsonPropertyName("add_images")]
-    public bool AddImages { get; set; } = true;
+    public bool AddImages { get; set; } = false;
 }
