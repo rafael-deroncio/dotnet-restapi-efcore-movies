f3c0c8da183b992c273d5b8b3a550d653323d640
Rafael Deroncio <rafael.deroncio@hotmail.com>
2024-08-21 21:26:05 -0300

    add attrs in ImageResponse

--- src/MovieMania.Domain/Responses/ImageResponse.cs
+++ src/MovieMania.Domain/Responses/ImageResponse.cs
@@ -1,6 +1,12 @@
+using System.Text.Json.Serialization;
+
 namespace MovieMania.Domain.Responses;
 
 public record ImageResponse
 {
-    
+    [JsonPropertyName("id")]
+    public int Id { get; set; }
+
+    [JsonPropertyName("image_url")]
+    public string ImageUrl { get; set; }
 }
