43849c7d1e3cd7c67b858852cc4b55b6e258ae51
Rafael Deroncio <rafael.deroncio@hotmail.com>
2024-08-21 21:26:29 -0300

    refactor attrs to virtual type

--- src/MovieMania.Core/Contexts/MovieManiaContext.cs
+++ src/MovieMania.Core/Contexts/MovieManiaContext.cs
@@ -6,24 +6,24 @@ namespace MovieMania.Core.Contexts;
 
 public class MovieManiaContext(DbContextOptions<MovieManiaContext> options) : DbContext(options)
 {
-    public DbSet<CountryEntity> Countries { get; set; }
-    public DbSet<DepartmentEntity> Departments { get; set; }
-    public DbSet<GenderEntity> Genders { get; set; }
-    public DbSet<GenreEntity> Genres { get; set; }
-    public DbSet<KeywordEntity> Keywords { get; set; }
-    public DbSet<LanguageEntity> Languages { get; set; }
-    public DbSet<LanguageRoleEntity> LanguageRoles { get; set; }
-    public DbSet<MovieCastEntity> MovieCasts { get; set; }
-    public DbSet<MovieCompanyEntity> MovieCompanies { get; set; }
-    public DbSet<MovieCrewEntity> MovieCrews { get; set; }
-    public DbSet<MovieGenreEntity> MovieGenres { get; set; }
-    public DbSet<MovieImageEntity> MovieImages { get; set; }
-    public DbSet<MovieKeywordEntity> MovieKeywords { get; set; }
-    public DbSet<MovieLanguageEntity> MovieLanguages { get; set; }
-    public DbSet<MovieEntity> Movies { get; set; }
-    public DbSet<PersonEntity> Persons { get; set; }
-    public DbSet<ProductionCompanyEntity> ProductionCompanies { get; set; }
-    public DbSet<ProductionCountryEntity> ProductionCountries { get; set; }
+    public virtual DbSet<CountryEntity> Countries { get; set; }
+    public virtual DbSet<DepartmentEntity> Departments { get; set; }
+    public virtual DbSet<GenderEntity> Genders { get; set; }
+    public virtual DbSet<GenreEntity> Genres { get; set; }
+    public virtual DbSet<KeywordEntity> Keywords { get; set; }
+    public virtual DbSet<LanguageEntity> Languages { get; set; }
+    public virtual DbSet<LanguageRoleEntity> LanguageRoles { get; set; }
+    public virtual DbSet<MovieCastEntity> MovieCasts { get; set; }
+    public virtual DbSet<MovieCompanyEntity> MovieCompanies { get; set; }
+    public virtual DbSet<MovieCrewEntity> MovieCrews { get; set; }
+    public virtual DbSet<MovieGenreEntity> MovieGenres { get; set; }
+    public virtual DbSet<MovieImageEntity> MovieImages { get; set; }
+    public virtual DbSet<MovieKeywordEntity> MovieKeywords { get; set; }
+    public virtual DbSet<MovieLanguageEntity> MovieLanguages { get; set; }
+    public virtual DbSet<MovieEntity> Movies { get; set; }
+    public virtual DbSet<PersonEntity> Persons { get; set; }
+    public virtual DbSet<ProductionCompanyEntity> ProductionCompanies { get; set; }
+    public virtual DbSet<ProductionCountryEntity> ProductionCountries { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
